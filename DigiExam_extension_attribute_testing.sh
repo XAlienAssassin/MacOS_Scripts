50e42d773ac852580690d6ad18f4c6cf8561334f
Orion Medina <31485978+XAlienAssassin@users.noreply.github.com>
2024-03-06 10:24:14 -0500

    changed the digiexam version to the the plist file destination to find the currently installed version. added the curl command the find the latest version

--- DigiExam_extension_attribute_testing.sh
+++ DigiExam_extension_attribute_testing.sh
@@ -25,10 +25,11 @@ info_plist="/Applications/DigiExam.app/Contents/Info.plist"
 # Check if Info.plist file exists
 if [ -e "$info_plist" ]; then
     # Extract the version information from the plist using PlistBuddy
-    digiexam_version="15.22.9"
+    digiexam_version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$info_plist")
 
     # Extract the latest version using the specified curl command
-    latest_version="15.0.99"
+    latest_version=$(curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" -fs https://support.digiexam.se/hc/en-us/articles/7119593625628-Client-updates | perl -ne 'print if /Version(?!.*Only(?!.*Mac))(?=.*Mac)?/' | head -1 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
+
     # Check if the latest version is empty (curl command failed)
     if [ -z "$latest_version" ]; then
         echo "<result>Curl command failed. Unable to retrieve the latest version.</result>"
