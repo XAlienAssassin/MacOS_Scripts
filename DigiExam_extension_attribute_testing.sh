f69d9dbb5ff90b4638d76538d8e8452158a8bf2d
Orion Medina <31485978+XAlienAssassin@users.noreply.github.com>
2024-03-06 10:02:07 -0500

    changed the version comparing to a shorter version

--- DigiExam_extension_attribute_testing.sh
+++ DigiExam_extension_attribute_testing.sh
@@ -4,31 +4,19 @@
 # Function to compare version numbers
 # Takes two version strings as input and compares them
 version_compare() {
-    # Split version numbers into arrays based on '.' delimiter
+    # Set '.' as delimiter to split version strings into arrays
     local IFS="."
-    local i version1=($1) version2=($2)
-    # Pad the shorter version with zeros to match the length of the longer version
-    while [ ${#version1[@]} -lt ${#version2[@]} ]; do
-        version1+=("0")
-    done
-    while [ ${#version2[@]} -lt ${#version1[@]} ]; do
-        version2+=("0")
-    done
-    # Compare each segment of the version numbers
+    local version1=($1) version2=($2)
+    # Pad the shorter version with zeros for equal length comparison
+    for ((i=${#version1[@]}; i<${#version2[@]}; i++)); do version1[i]=0; done
+    for ((i=${#version2[@]}; i<${#version1[@]}; i++)); do version2[i]=0; done
+    # Loop through each segment to compare version numbers
     for ((i=0; i<${#version1[@]}; i++)); do
-        # Comparing version numbers as base-10 integers to avoid octal interpretation of leading zeros
-        # If a segment of version1 is greater than the corresponding segment of version2, version1 is newer
-        if ((10#${version1[i]} > 10#${version2[i]})); then
-            echo ">"
-            return
-        # If a segment of version1 is less than the corresponding segment of version2, version1 is older
-        elif ((10#${version1[i]} < 10#${version2[i]})); then
-            echo "<"
-            return
-        fi
+        # Compare segments as base-10 to prevent octal interpretation
+        if ((10#${version1[i]} > 10#${version2[i]})); then echo ">"; return; fi  # version1 is newer
+        if ((10#${version1[i]} < 10#${version2[i]})); then echo "<"; return; fi  # version1 is older
     done
-    # If all segments are equal, the versions are the same
-    echo "="
+    echo "="  # Versions are identical if all segments are equal
 }
 
 # Path to DigiExam Info.plist file (modify as needed)
@@ -37,10 +25,10 @@ info_plist="/Applications/DigiExam.app/Contents/Info.plist"
 # Check if Info.plist file exists
 if [ -e "$info_plist" ]; then
     # Extract the version information from the plist using PlistBuddy
-    digiexam_version="15.0.1"
+    digiexam_version="15.22.9"
 
     # Extract the latest version using the specified curl command
-    latest_version="15.0.10"
+    latest_version="15.03.99"
     # Check if the latest version is empty (curl command failed)
     if [ -z "$latest_version" ]; then
         echo "<result>Curl command failed. Unable to retrieve the latest version.</result>"
