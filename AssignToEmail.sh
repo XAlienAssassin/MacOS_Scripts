1f9ef7805412d2086f285b842fcbf15087c70fcb
Orion Medina <31485978+XAlienAssassin@users.noreply.github.com>
2024-10-23 10:59:34 -0400

    updated assigning based on created account
    added the ability to just use the username for the account instead of checking the first and last name if the account was created with Jamf connect.

--- AssignToEmail.sh
+++ AssignToEmail.sh
@@ -9,21 +9,27 @@ full_name=$(sudo /usr/libexec/PlistBuddy -c "Print :realname:0" "/var/db/dslocal
 echo $last_user
 echo $full_name
 
-# Extract the first name and last name using awk from the last_user.plist file
-if [[ -n $full_name ]]; then
-	first_name=$(echo $full_name | awk '{print $1}' | tr -d "-" | tr -d "'")
-	last_name=$(echo $full_name | awk '{print $2}' | tr -d "-" | tr -d "'")
-    last_name2=$(echo $full_name | awk '{print $3}' | tr -d "-" | tr -d "'")
-
-	# Insert a words between the first name and last name and add domain
-	modified_name="$first_name.$last_name$last_name2@saintandrews.net"
-
-	# Echo the modified name (test purpose)
-	echo $modified_name
-    echo "Script ran successfully!"
-    
-    # change the assigned username to the modified_name in jamf
-	#jamf recon -endUsername $modified_name
+# Check if the username already contains a "."
+if [[ $last_user == *"."* ]]; then
+    modified_name="${last_user}@saintandrews.net"
 else
-	echo "Script encountered error"
+    # Extract the first name and last name using awk from the last_user.plist file
+    if [[ -n $full_name ]]; then
+        first_name=$(echo $full_name | awk '{print $1}' | tr -d "-" | tr -d "'")
+        last_name=$(echo $full_name | awk '{print $2}' | tr -d "-" | tr -d "'")
+        last_name2=$(echo $full_name | awk '{print $3}' | tr -d "-" | tr -d "'")
+
+        # Insert a words between the first name and last name and add domain
+        modified_name="$first_name.$last_name$last_name2@saintandrews.net"
+    else
+        echo "Script encountered error"
+        exit 1
+    fi
 fi
+
+# Echo the modified name (test purpose)
+echo $modified_name
+echo "Script ran successfully!"
+
+# change the assigned username to the modified_name in jamf
+jamf recon -endUsername $modified_name
