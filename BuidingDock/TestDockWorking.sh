2bcb9bd442e88242592d68a13362ff91b0692ed9
Orion Medina <31485978+XAlienAssassin@users.noreply.github.com>
2024-11-08 14:07:20 -0500

    added more apps to check test

--- BuidingDock/TestDockWorking.sh
+++ BuidingDock/TestDockWorking.sh
@@ -8,12 +8,18 @@ SEARCH_TEXT_2='
                 "_CFURLString" = "file:///System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app/";
 '
 
+SEARCH_TEXT_3='
+                "_CFURLString" = "file:///Applications/Google%20Chrome.app";
+'
+
 
 # Read the contents of the plist file
 PLIST_CONTENTS=$(defaults read com.apple.dock persistent-apps)
 
-# Check if both snippets of text are present in the plist content
-if [[ "$PLIST_CONTENTS" == *"$SEARCH_TEXT_1"* ]]; then
+# Check if all snippets of text are present in the plist content
+if [[ "$PLIST_CONTENTS" == *"$SEARCH_TEXT_1"* ]] && \
+   [[ "$PLIST_CONTENTS" == *"$SEARCH_TEXT_2"* ]] && \
+   [[ "$PLIST_CONTENTS" == *"$SEARCH_TEXT_3"* ]]; then
   echo "<result>Enabled</result>"
 else
   echo "<result>Not Enabled</result>"
