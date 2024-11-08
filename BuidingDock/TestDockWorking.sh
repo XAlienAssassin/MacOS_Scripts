d052b6afb30448ad6398ba09fb1852ab5033754b
Orion Medina <31485978+XAlienAssassin@users.noreply.github.com>
2024-11-08 14:14:52 -0500

    added file label instead of label location

--- BuidingDock/TestDockWorking.sh
+++ BuidingDock/TestDockWorking.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 SEARCH_TEXT_1='
-                "_CFURLString" = "file:///Applications/Self%20Service.app/";
+            "file-label" = Safari;
 '
 
 SEARCH_TEXT_2='
-                "_CFURLString" = "file:///System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app/";
+            "file-label" = "Self Service";
 '
 
 SEARCH_TEXT_3='
-                "_CFURLString" = "file:///Applications/Google%20Chrome.app";
+            "file-label" = "Google Chrome";
 '
 
 
 # Read the contents of the plist file
 PLIST_CONTENTS=$(defaults read com.apple.dock persistent-apps)
 
-# Check if all snippets of text are present in the plist content
+# Check if all three snippets of text are present in the plist content
 if [[ "$PLIST_CONTENTS" == *"$SEARCH_TEXT_1"* ]] && \
    [[ "$PLIST_CONTENTS" == *"$SEARCH_TEXT_2"* ]] && \
    [[ "$PLIST_CONTENTS" == *"$SEARCH_TEXT_3"* ]]; then
